b4b28d9ce6ca91815facf797a6e54089e6864522
surakat gideon <surakatg@gmail.com>
2022-05-31 21:52:22 +0100

    repository

---
+++ HRLeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using HRLeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository :    IGenericRepository<LeaveRequest> 
+
+    {
+
+
+    }
+}
---
+++ HRLeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using HRLeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
